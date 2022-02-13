7c46f19b7ad2dc1e8aca8244590958f6c427f459
shekerhack <luckysheker.92@gmail.com>
2022-02-13 16:22:11 -0600

    git add .

--- src/Homeworks/Homework02.java
+++ src/Homeworks/Homework02.java
@@ -22,27 +22,30 @@ public class Homework02 {
 
         System.out.println("What is your name?");
         String userFirstName = userInfo.next();
-        System.out.println("User first name is = " + userFirstName);
 
         System.out.println("What is your last name?");
         String userLastName = userInfo.next();
-        System.out.println("User last name is = " + userLastName);
 
         System.out.println("What is your age?");
         int userAge = userInfo.nextInt();
-        System.out.println("User age is = " + userAge);
 
         System.out.println("What is your email address?");
         String userEmail = userInfo.next();
-        System.out.println("User email address is = " + userEmail);
+        userInfo.nextLine();
 
         System.out.println("What is your phone number?");
-        String userPhoneNumber = userInfo.next();
-        System.out.println("User phone number is = " + userPhoneNumber);
+        String userPhoneNumber = userInfo.nextLine();
 
         System.out.println("What is your address?");
-        String userAddress = userInfo.next();
-        System.out.println("User address is = " + userAddress);
+        String userAddress = userInfo.nextLine();
+
+        /*User who joined this program is John Doe. John’s age is 45. John’s email address is johndoe@gmail.com, phone number (123) 123 1234, and address is 123 St Chicago IL 12345.
+
+         */
+
+        System.out.println("\tUser who joined this program is " + userFirstName + " " + userLastName + ". John's age is " +
+                userAge + ". John's email \naddress is " + userAddress + ",phone number is " + userPhoneNumber + ", and address is \n" + userAddress + ".");
+
 
         /* Task-2 Write a program that asks user their favorite book, color, and number in separate statements.
                   Store those data in proper data types
@@ -58,14 +61,21 @@ public class Homework02 {
 
         Scanner userFavorites = new Scanner(System.in);
         System.out.println("What is your  favorite book?");
-        String userFavoriteBook = userFavorites.next();
+        String userFavoriteBook = userFavorites.nextLine();
 
         System.out.println("What is your favorite color?");
         String userFavColor = "\"" + userFavorites.next() + "\"";
 
+
         System.out.println("What is your favorite number");
         int userFavNumber = userFavorites.nextInt();
 
-        System.out.println("User favorite book is = " + userFavoriteBook + "\nUser favorite color is = " + userFavColor.concat("\nUser favorite number is = " + userFavNumber));
+         /*User’s favorite book is: The Brothers Karamazov
+          User’s favorite color is: Blue
+          User’s favorite number is: 7
+
+         */
+
+        System.out.println("User's favorite book is: " + userFavoriteBook + "\nUser's favorite color is: " + userFavColor.concat("\nUser's favorite number is: " + userFavNumber));
     }
 }
